2b4352b5487da3e689c7a9ada2ba264c752673c6
David Baur <david.baur@unic.com>
2021-03-05 15:39:42 +0100

    intentionally adding format method with concrete paramtype wordExistenceMap in order to refactor later to general Map Type

--- src/main/java/base/notes/spellcheck/formatter/OverviewSpellCheckerResultFormatter.java
+++ src/main/java/base/notes/spellcheck/formatter/OverviewSpellCheckerResultFormatter.java
@@ -1,9 +1,11 @@
 package base.notes.spellcheck.formatter;
 
+import base.notes.spellcheck.model.WordExistenceMap;
+
 public class OverviewSpellCheckerResultFormatter {   //InterfacePotential
     public OverviewSpellCheckerResultFormatter() {
     }
-    public String format(){
+    public String format(WordExistenceMap wordExistenceMap){
         return "";
     }
 }
--- src/main/java/base/notes/spellcheck/formatter/SingleNoteSpellCheckerResultFormatter.java
+++ src/main/java/base/notes/spellcheck/formatter/SingleNoteSpellCheckerResultFormatter.java
@@ -1,7 +1,12 @@
 package base.notes.spellcheck.formatter;
 
+import base.notes.spellcheck.model.WordExistenceMap;
+
 public class SingleNoteSpellCheckerResultFormatter {
     public SingleNoteSpellCheckerResultFormatter() {
 
+    }
+    public String format(WordExistenceMap wordExistenceMap){
+        return "";
     }
 }
