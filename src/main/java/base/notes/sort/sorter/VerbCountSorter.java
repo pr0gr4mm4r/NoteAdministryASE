e9ff5c360fddd634e469d9d99751f15b81fbfe2b
pr0gr4mm4r <45204482+pr0gr4mm4r@users.noreply.github.com>
2021-03-09 22:08:39 +0100

    Starting with VerbCountSorter

---
+++ src/main/java/base/notes/sort/sorter/VerbCountSorter.java
@@ -0,0 +1,10 @@
+package base.notes.sort.sorter;
+
+import base.interfaces.Sorter;
+
+public class VerbCountSorter implements Sorter {
+    @Override
+    public String sort() {
+        return null;
+    }
+}
