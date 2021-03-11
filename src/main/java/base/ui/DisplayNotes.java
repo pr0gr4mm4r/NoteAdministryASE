e8270237b0937e590adaba1dfa1c3beee06806eb
David Baur <david.baur@unic.com>
2021-03-11 10:03:00 +0100

    progressing with Gui

--- src/main/java/base/ui/DisplayNotes.java
+++ src/main/java/base/ui/DisplayNotes.java
@@ -12,18 +12,25 @@ import static base.config.Globals.path_for_notes;
 public class DisplayNotes extends JFrame {
     private List<JLabel> noteNameLabels = new ArrayList<>();
     public DisplayNotes() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
+        JPanel masterPanel = new JPanel();
+        GridLayout gridLayout = new GridLayout(5, 1);
+        masterPanel.setLayout(gridLayout);
+        JPanel header = new JPanel();
+        JPanel capturePanel = new JPanel();
+        JPanel notePanel = new JPanel();
+        capturePanel.setLayout(new FlowLayout());
         JLabel label = new JLabel("Overview of all notes in directory" + path_for_notes);
-        panel.add(label);
+        capturePanel.add(label);
         MultiNoteProcessor multiNoteProcessor = new MultiNoteProcessor();
         List<String> noteNames = new ArrayList<>(multiNoteProcessor.getNoteNames());
         for(int i = 0; i < noteNames.size(); i++){
             JLabel jLabel = new JLabel(noteNames.get(i));
             noteNameLabels.add(jLabel);
-            panel.add(noteNameLabels.get(i));
+            notePanel.add(noteNameLabels.get(i));
         }
-        this.add(panel);
+        masterPanel.add(capturePanel);
+        masterPanel.add(notePanel);
+        this.add(masterPanel);
         this.setSize(800, 500);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
