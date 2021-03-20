41a4edfa4de1cd81d604914e85009d7be36b1385
David Baur <david.baur@unic.com>
2021-03-20 11:51:45 +0100

    start with OverviewWordFinderTest

---
+++ src/test/java/note/find/OverviewWordFinderTest.java
@@ -0,0 +1,19 @@
+package note.find;
+
+import org.junit.Test;
+
+import javax.mail.MessagingException;
+
+
+public class OverviewWordFinderTest {
+
+    @Test
+    public void ja() throws MessagingException {
+
+    }
+
+    @Test
+    public void nein() throws MessagingException {
+
+    }
+}
---
+++ src/test/java/note/find/SingleNoteWordFinderTest.java
@@ -0,0 +1,20 @@
+package note.find;
+
+import org.junit.Test;
+
+import javax.mail.MessagingException;
+
+public class SingleNoteWordFinderTest {
+
+    @Test
+    public void blub() throws MessagingException {
+
+    }
+
+    @Test
+    public void bla() throws MessagingException {
+
+    }
+}
+
+
