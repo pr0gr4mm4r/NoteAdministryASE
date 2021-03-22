caf4f32fab93ebb41b16168ec16bf3dc86fa51a9
pr0gr4mm4r <45204482+pr0gr4mm4r@users.noreply.github.com>
2021-03-22 17:42:23 +0100

    refactoring Sorter

--- src/main/java/base/notes/sort/sorter/VerbCountSorter.java
+++ src/main/java/base/notes/sort/sorter/VerbCountSorter.java
@@ -16,48 +16,47 @@ import static base.config.Globals.scanner;
 public class VerbCountSorter implements Sorter {
     @Override
     public Map initialize() {
-        return null;
-    }
-
-    @Override
-    public List sort(Map map) {
         MultiNoteProcessor multiNoteProcessor = new MultiNoteProcessor(path_for_notes);
         List<String[]> noteList = multiNoteProcessor.getWordListList();
         List<String> nameList = new ArrayList<>(multiNoteProcessor.getNoteNames());
         StringIntegerMap<String, Integer> verbCountMap = new StringIntegerMap<>();
         for (int i = 0; i < noteList.size(); i++) {
             int verbCounter = 0;
-            for(int j = 0; j < noteList.get(i).length; j++){
-                if(RiTa.isVerb(noteList.get(i)[j])){
+            for (int j = 0; j < noteList.get(i).length; j++) {
+                if (RiTa.isVerb(noteList.get(i)[j])) {
                     verbCounter++;
                 }
             }
             String nameOfNote = nameList.get(i);
             verbCountMap.put(nameOfNote, verbCounter);
         }
+        return verbCountMap;
+    }
+
+    @Override
+    public List sort(Map verbCountMap) {
         Set<Entry<String, Integer>> verbCountSet = verbCountMap.entrySet();
         List<Entry<String, Integer>> finalVerbCountList = new ArrayList<>(verbCountSet);
         Comparator<Entry<String, Integer>> valueComparator = Comparator.comparingInt(Entry::getValue);
         finalVerbCountList.sort(valueComparator);
-        WordCountNoteSorterResultFormatter wordCountNoteSorterResultFormatter = new WordCountNoteSorterResultFormatter();
-        String result = wordCountNoteSorterResultFormatter.formatList(finalVerbCountList);
-        System.out.println(result);
-        System.out.println("Do you want to save the Output as a Logfile?");
-        System.out.println("Type 'yes' without '' to confirm or type anything else to abort:");
-        String confirmation = scanner.nextLine();
-        if(confirmation.equals("yes")){
-            new LogFileDeclarator(result, "Sorting Notes by Quantity of Verbs");
-        }
-        return null;
+        return finalVerbCountList;
     }
 
     @Override
-    public String format(List result) {
-        return null;
+    public String format(List finalVerbCountList) {
+        WordCountNoteSorterResultFormatter wordCountNoteSorterResultFormatter = new WordCountNoteSorterResultFormatter();
+        String result = wordCountNoteSorterResultFormatter.formatList(finalVerbCountList);
+        return result;
     }
 
     @Override
     public void print(String formattedResult) {
-
+        System.out.println(formattedResult);
+        System.out.println("Do you want to save the Output as a Logfile?");
+        System.out.println("Type 'yes' without '' to confirm or type anything else to abort:");
+        String confirmation = scanner.nextLine();
+        if (confirmation.equals("yes")) {
+            new LogFileDeclarator(formattedResult, "Sorting Notes by Quantity of Verbs");
+        }
     }
 }
