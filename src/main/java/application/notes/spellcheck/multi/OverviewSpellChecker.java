08ff92fde4fca96bdb236448f85b0369a1aaeaee
David Baur <david.baur@unic.com>
2021-03-23 15:32:14 +0100

    refactor OverviewSpellChecker

--- src/main/java/application/notes/spellcheck/multi/OverviewSpellChecker.java
+++ src/main/java/application/notes/spellcheck/multi/OverviewSpellChecker.java
@@ -16,13 +16,20 @@ import static application.notes.spellcheck.single.SingleNoteSpellChecker.calcula
 public class OverviewSpellChecker {
     private final WordExistenceMapList wordExistenceMapList = new WordExistenceMapList();
     private final OverviewSpellCheckerResultFormatter overviewSpellCheckerResultFormatter = new OverviewSpellCheckerResultFormatter();
+    private final List<Double> percentageValueList = new ArrayList<>();
 
 
     public OverviewSpellChecker() {
         final MultiNoteProcessor multiNoteProcessor = new MultiNoteProcessor(path_for_notes);
         final List<String[]> wordListList = multiNoteProcessor.getWordListList();
         wordExistenceMapList.fill(wordListList);
-        List<Double> percentageValueList = new ArrayList<>();
+        fillPercentageValueList(percentageValueList, wordListList);
+        String result = format(overviewSpellCheckerResultFormatter, percentageValueList);
+        print(result);
+        openLogFileDialogue(result);
+    }
+
+    private void fillPercentageValueList(List<Double> percentageValueList, List<String[]> wordListList) {
         for (String[] wordList : wordListList) {
             double wordCountOfNote = wordList.length;
             SpellCheckerRaw spellCheckerRaw = new SpellCheckerRaw(wordList);
@@ -30,15 +37,24 @@ public class OverviewSpellChecker {
             double percentageValue = calculatePercentageWiseOccurrence(wordCountOfNote, wordsInLexikon);
             percentageValueList.add(percentageValue);
         }
-        String result = overviewSpellCheckerResultFormatter.formatList(wordExistenceMapList, percentageValueList);
-        System.out.println(result);
-        System.out.println();
+    }
+
+    private void openLogFileDialogue(String result) {
         System.out.println("Do you want to save the output as a logfile?");
         System.out.println("Type 'yes' without '' to confirm or type anything else to abort:");
         String confirmation = scanner.nextLine();
-        if(confirmation.equals("yes")){
+        if (confirmation.equals("yes")) {
             new LogFileDeclarator(result, "Spellcheck All Notes");
         }
+    }
+
+    private void print(String result) {
+        System.out.println(result);
+        System.out.println();
+    }
+
+    private String format(OverviewSpellCheckerResultFormatter overviewSpellCheckerResultFormatter, List<Double> percentageValueList) {
+        return overviewSpellCheckerResultFormatter.formatList(wordExistenceMapList, percentageValueList);
     }
 
     public WordExistenceMapList getWordExistenceMapList() {
