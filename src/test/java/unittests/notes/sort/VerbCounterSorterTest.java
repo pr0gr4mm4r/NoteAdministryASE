a2fba863c823abaa57f70e469f0c64a51a0a80d3
pr0gr4mm4r <45204482+pr0gr4mm4r@users.noreply.github.com>
2021-03-24 15:20:07 +0100

    removing unnecessary imports

--- src/test/java/unittests/notes/sort/VerbCounterSorterTest.java
+++ src/test/java/unittests/notes/sort/VerbCounterSorterTest.java
@@ -12,15 +12,6 @@ public class VerbCounterSorterTest {
 
     @Test
     public void increaseCounterForEachRhymeTest() throws IOException {
-      /*  File file1 = temporaryFolder.newFile("myfile1.txt");
-        File file2 = temporaryFolder.newFile("myfile2.txt");
-        String testContent = "fine line whine";
-        String testContent2 = "I like flowers";
-        writingTestContentToArtificialFile(file1, testContent);
-        writingTestContentToArtificialFile(file2, testContent2);
-        List<Entry<String, Integer>> wordsInLexiconEntries = new ArrayList<>();
-        int counter = 0;
-        MultiNoteProcessor multiNoteProcessor = new MultiNoteProcessor();
-        RhymesNoteSorter rhymesNoteSorter = new RhymesNoteSorter(multiNoteProcessor);*/
+
     }
 }
