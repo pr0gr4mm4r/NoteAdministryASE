ee25ddc1f68ca5fb8655e880c09f3e234451b0ce
David Baur <david.baur@unic.com>
2021-03-28 19:01:13 +0200

    remove comment

--- src/main/java/application/notes/sort/abstraction/NoteSorter.java
+++ src/main/java/application/notes/sort/abstraction/NoteSorter.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface NoteSorter {
-    Map initialize() throws NoFilesInDirectoryException, IOException; // z.B. keine konkrete Map Implementierung
+    Map initialize() throws NoFilesInDirectoryException, IOException;
     List sort(Map map);
     String format(List result);
     void dialogue(String formattedResult) throws IOException;
