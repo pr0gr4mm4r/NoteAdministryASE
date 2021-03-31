2c7190f741426b00e52eb835b3c4f48905c3e943
David Baur <david.baur@unic.com>
2021-03-31 14:59:41 +0200

    pure fabrication example

--- src/main/java/utility/calc/PercentageCalculator.java
+++ src/main/java/utility/calc/PercentageCalculator.java
@@ -2,11 +2,11 @@ package utility.calc;
 
 public class PercentageCalculator {
 
-    public PercentageCalculator() {
+    public PercentageCalculator() { // Pure Fabrication wird verwendet, um mathematischen Sachverhalt vom Domänenwissen zu trennen
     }
 
-    public static double calculatePercentageWiseOccurrence(final double wordCount, final double wordsInLexikon) {
-        final double resultInPercent = (wordsInLexikon / wordCount) * 100;
+    public static double calculatePercentageWiseOccurrence(final double actualCount, final double totalOccurrence) {
+        final double resultInPercent = (totalOccurrence / actualCount) * 100;
         return resultInPercent;
     }
 }
