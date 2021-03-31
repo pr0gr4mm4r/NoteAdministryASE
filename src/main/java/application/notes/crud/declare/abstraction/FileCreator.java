c8e9ca4c894b79a1e2a4ddd2e93e445ea73c79a8
David Baur <david.baur@unic.com>
2021-03-31 14:38:11 +0200

    example Interface which can lead to Polymorphism

--- src/main/java/application/notes/crud/declare/abstraction/FileCreator.java
+++ src/main/java/application/notes/crud/declare/abstraction/FileCreator.java
@@ -2,6 +2,7 @@ package application.notes.crud.declare.abstraction;
 
 import java.nio.file.Path;
 
-public interface FileCreator {
+public interface FileCreator { // Einführung eines Interfaces führt z.B. zu Polymorphie, da jede Klasse,
+    // die das Interface in Zukunft implementiert, eine eigene Implementierung anbieten muss
     boolean tryToCreateFile(final Path completePath);
 }
